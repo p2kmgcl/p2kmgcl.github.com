63090ae6131c4bba5ed06f031ad93dac3790147a
Pablo Molina <p2kmgcl@gmail.com>
2013-12-12 18:12:34 +0100

    Añadido el registro de teclado y el harlem shake para probarlo

--- js/main.js
+++ js/main.js
@@ -8,3 +8,7 @@ $('html').removeClass('no-js');
 {% include scripts/ajaxload.js %}
 {% include scripts/themechange.js %}
 {% include scripts/rolechange.js %}
+
+{% include scripts/harlemshake.js %}
+
+{% include scripts/keycodegetter.js %}
