da2a82aaaaa63b39a068cd2c00df034bfa28b52b
Pablo Molina <contact@pablomolina.me>
2019-08-15 20:28:02 +0200

    Update chachi-shell.sh

--- public/chachi-shell.sh
+++ public/chachi-shell.sh
@@ -6,4 +6,4 @@ sudo apt-add-repository -y ppa:ansible/ansible 1> /dev/null
 sudo apt-get update -y 1> /dev/null
 sudo apt-get install ansible -q -y 1> /dev/null
 
-ansible-pull --url https://github.com/p2kmgcl/chachi-shell.git --directory $HOME/.chachi-shell --only-if-changed
+ansible-pull --url https://github.com/p2kmgcl/chachi-shell.git --directory $HOME/.chachi-shell --only-if-changed playbook.yml
