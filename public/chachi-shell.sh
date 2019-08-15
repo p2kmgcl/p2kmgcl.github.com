e73dbf223dc21172a8062e7c0222fd9ca8010c90
Pablo Molina <contact@pablomolina.me>
2019-08-15 20:32:34 +0200

    Update chachi-shell.sh

--- public/chachi-shell.sh
+++ public/chachi-shell.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update -y 1> /dev/null
-sudo apt-get install -y software-properties-common curl git 1> /dev/null
-sudo apt-add-repository -y ppa:ansible/ansible 1> /dev/null
-sudo apt-get update -y 1> /dev/null
-sudo apt-get install ansible -q -y 1> /dev/null
+if hash ansible 2>/dev/null; then
+  echo Ansible installed
+else
+  echo Installing ansible
+  sudo apt-get update -y 1> /dev/null
+  sudo apt-get install -y software-properties-common curl git 1> /dev/null
+  sudo apt-add-repository -y ppa:ansible/ansible 1> /dev/null
+  sudo apt-get update -y 1> /dev/null
+  sudo apt-get install ansible -q -y 1> /dev/null
+fi
 
+echo Running playbook
 ansible-pull --url https://github.com/p2kmgcl/chachi-shell.git --directory $HOME/.chachi-shell --only-if-changed playbook.yml
