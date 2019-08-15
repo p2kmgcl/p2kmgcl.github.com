e11c43f4f0d7e8e5560daa6b9b6dd486fb6406bf
Pablo Molina <contact@pablomolina.me>
2019-08-15 19:51:31 +0200

    Update chachi-shell.sh
    Use apt-get instead of apt

--- public/chachi-shell.sh
+++ public/chachi-shell.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt update -y 1> /dev/null
-sudo apt install -y software-properties-common curl git 1> /dev/null
+sudo apt-get update -y 1> /dev/null
+sudo apt-get install -y software-properties-common curl git 1> /dev/null
 sudo apt-add-repository -y ppa:ansible/ansible 1> /dev/null
 sudo apt-get update -y 1> /dev/null
-sudo apt install ansible -q -y 1> /dev/null
+sudo apt-get install ansible -q -y 1> /dev/null
 
 ansible-pull --url https://github.com/p2kmgcl/chachi-shell.git --directory $HOME/.chachi-shell --only-if-changed
