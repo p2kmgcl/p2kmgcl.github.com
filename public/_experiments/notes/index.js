fc15643f0ee212db72e487af9553093c990c8883
Pablo Molina <contact@pablomolina.me>
2020-07-19 18:50:35 +0200

    Rethink theme and remove experiments

--- public/_experiments/notes/index.js
+++
@@ -1,9 +0,0 @@
-const LOCAL_STORAGE_ID = 'experiment_notes';
-const textarea = document.querySelector('textarea');
-
-textarea.value = localStorage.getItem(LOCAL_STORAGE_ID) || '';
-textarea.focus();
-
-textarea.addEventListener('keyup', () => {
-  localStorage.setItem(LOCAL_STORAGE_ID, textarea.value);
-});
--- public/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('activate', (event) =>
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheIds) =>
-        Promise.all(cacheIds.map((cacheId) => caches.delete(cacheId))),
-      ),
-  ),
-);
