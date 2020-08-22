12da59e5e4e4ad479352077bb26d52faf8c94610
Pablo Molina <contact@pablomolina.me>
2020-08-22 16:58:05 +0200

    Simplify dark theme animation (rely on CSS)

---
+++ public/src/dark-theme-animation.js
@@ -0,0 +1,42 @@
+function bindButton() {
+  const button = document.getElementById('darkThemeButton');
+
+  button.addEventListener('click', async () => {
+    const toDark = button.getAttribute('aria-pressed') !== 'true';
+
+    button.disabled = true;
+
+    if (matchMedia('(prefers-reduced-motion: reduce)')) {
+      await new Promise((resolve) => {
+        const tone = toDark ? 34 : 240;
+        const color = `rgb(${tone}, ${tone}, ${tone})`;
+        const transparent = `rgba(${tone}, ${tone}, ${tone}, 0)`;
+
+        button.style.transition = 'none';
+        button.style.boxShadow = `0 0 0 0 ${color}`;
+
+        const onTransitionEnd = () => {
+          button.removeEventListener('transitionend', onTransitionEnd);
+
+          button.style.transition =
+            'box-shadow ease var(--transition-duration) var(--transition-duration)';
+          button.style.boxShadow = `0 0 0 150vmax ${transparent}`;
+
+          resolve();
+        };
+
+        setTimeout(() => {
+          button.style.transition = 'box-shadow ease-in 1s';
+          button.style.boxShadow = `0 0 0 150vmax ${color}`;
+          button.addEventListener('transitionend', onTransitionEnd);
+        }, 100);
+      });
+    }
+
+    document.body.classList.toggle('dark', toDark);
+    button.setAttribute('aria-pressed', toDark ? 'true' : 'false');
+    button.disabled = false;
+  });
+}
+
+bindButton();
