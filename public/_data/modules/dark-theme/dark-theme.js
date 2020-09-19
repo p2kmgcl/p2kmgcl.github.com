1a6a592873cef08c4ab14cf4165448d3f72e945b
Pablo Molina <contact@pablomolina.me>
2020-09-19 19:36:13 +0200

    Split code using jekyll includes

--- public/_data/modules/dark-theme/dark-theme.js
+++ public/_data/modules/dark-theme/dark-theme.js
@@ -1,3 +1,16 @@
+const { matches: prefersDark } = matchMedia('(prefers-color-scheme: dark)');
+
+if (prefersDark) {
+  const button = document.querySelector('button');
+  document.body.style.transition = 'none';
+  document.body.classList.add('dark');
+  button.setAttribute('aria-pressed', true);
+
+  requestAnimationFrame(() => {
+    document.body.style.transition = '';
+  });
+}
+
 function bindButton() {
   const button = document.getElementById('darkThemeButton');
 
