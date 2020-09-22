0a89e53b69ffa7524ac939ea00010d78c20885e7
Pablo Molina <contact@pablomolina.me>
2020-09-22 10:48:28 +0200

    Persist selected theme

--- public/_data/modules/dark-theme/dark-theme.js
+++ public/_data/modules/dark-theme/dark-theme.js
@@ -1,21 +1,9 @@
-const { matches: prefersDark } = matchMedia('(prefers-color-scheme: dark)');
-
-if (prefersDark) {
-  const button = document.querySelector('button');
-  document.body.style.transition = 'none';
-  document.body.classList.add('dark');
-  button.setAttribute('aria-pressed', true);
-
-  requestAnimationFrame(() => {
-    document.body.style.transition = '';
-  });
-}
-
 function bindButton() {
   const button = document.getElementById('darkThemeButton');
 
   button.addEventListener('click', async () => {
     const toDark = button.getAttribute('aria-pressed') !== 'true';
+    localStorage.setItem('selectedTheme', toDark ? 'dark' : 'light');
 
     button.disabled = true;
 
