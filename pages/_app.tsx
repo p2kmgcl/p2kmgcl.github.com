66620a5f5faa93d9a039328cbd62c50e0d9ecfdd
Pablo Molina <contact@pablomolina.me>
2021-06-29 17:48:00 +0200

    Remove identity if not needed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,7 +38,6 @@ const AppContent: FC<AppProps> = ({ Component, pageProps }) => {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 
         {[
           { tag: 'all', title: pkg.config.blogName },
