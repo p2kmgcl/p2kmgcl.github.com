ea2433a59e62f8114a1ffb5b59bf5e2c395b2c6f
Pablo Molina <contact@pablomolina.me>
2021-07-28 18:38:12 +0200

    Update links

--- pages/links.tsx
+++ pages/links.tsx
@@ -41,22 +41,29 @@ export default function Links() {
         <H3>Aside projects</H3>
 
         <Link
+          emoji="🧑‍🏫"
+          title="Teach[Tech] (2021-now)"
+          href="https://teacht3ch.com/"
+          description="Participating in this incredible nonprofit iniciative to teach people how to code using HTML, CSS and JavaScript."
+        />
+
+        <Link
           emoji="📺"
-          title="Twitch channel"
+          title="Twitch channel (2021-now)"
           href="https://twitch.tv/p2kmgcl"
           description="Videos about software development, mostly about FrontEnd, but also learning new programming languages and other interesting topics."
         />
 
         <Link
           emoji="🎩"
-          title="Seniore"
+          title="Seniore (2020-now)"
           href="https://www.npmjs.com/package/seniore"
           description="NodeJS based CLI that manages my regular GitHub and Jira workflow with small and opinionated scripts."
         />
 
         <Link
           emoji="👋"
-          title="Holi"
+          title="Holi (2020-now)"
           href="https://chrome.google.com/webstore/detail/jabgddbkjbekadednbljlbhhabiidndj"
           description="Chrome extension that overrides the new tab page allowing taking notes with some syntax sugar and seeing a minimal bookmark list."
         />
@@ -67,21 +74,21 @@ export default function Links() {
 
         <Link
           emoji="🎭"
-          title="Empowering Business Users to Create Sites and Pages With Full Autonomy"
+          title="Empowering Business Users to Create Sites and Pages With Full Autonomy (2019, talk)"
           href="https://www.youtube.com/watch?v=1LFUJH_5PJ0"
           description="Corporate presentation about new features that I was working on during 2019 in Liferay."
         />
 
         <Link
           emoji="🎞️"
-          title="60 Frames of Animations (Spanish talk)"
+          title="60 Frames of Animations (2019, Spanish talk)"
           href="https://www.youtube.com/watch?v=UqEVy4Sa7WI"
           description="Brief exploration about why animations are important in an application and a live coding of how we can optimize an existing animation with CSS and JS."
         />
 
         <Link
           emoji="👨‍💻"
-          title="Hands-on experience with Modern Site Building (workshop)"
+          title="Hands-on experience with Modern Site Building (2018, workshop)"
           href="https://www.youtube.com/watch?v=KpiQ5R8KruI"
           description="Corporate workshop speaking about 'Fragments', a new Liferay feature we published on 7.1 release."
         />
